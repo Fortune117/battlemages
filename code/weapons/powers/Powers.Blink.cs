abc33c507e3fb1d7f5a4c0c6608873c9a46edb8f
Fortune <7847372+Fortune117@users.noreply.github.com>
2023-01-25 19:01:10 +1100

    get blink functioning

---
+++ code/weapons/powers/Powers.Blink.cs
@@ -0,0 +1,122 @@
+﻿using Sandbox;
+
+namespace BattleMages;
+
+public partial class Powers
+{
+    private Particles BlinkTarget;
+    
+    private float BlinkRange => 375f;
+    private float BlinkFallRange => 90f;
+
+    private float BlinkSpeed => 2000f;
+    
+    [Net, Predicted]
+    private TimeUntil TimeUntilBlinkFinished { get; set; }
+    
+    [Net, Predicted]
+    private bool IsBlinking { get; set; }
+    
+    [Net, Predicted]
+    private Vector3 BlinkStartPosition { get; set; }
+    
+    [Net, Predicted]
+    private Vector3 BlinkEndPosition { get; set; }
+    
+    [ClientRpc]
+    private void CreateTargetVFX()
+    {
+        BlinkTarget?.Destroy();
+        BlinkTarget = Particles.Create("particles/powers/blink/blink_target.vpcf");
+    }
+    
+    private TraceResult GetBlinkTrace()
+    {
+        TraceResult tr;
+        
+        if (Game.IsClient)
+        {
+            tr = Trace.Ray(FortuneUtil.GetCameraRay(), BlinkRange)
+                .Ignore(Player)
+                .Radius(7f)
+                .WithoutTags(BMTags.PhysicsTags.Trigger)
+                .WorldAndEntities()
+                .Run();
+
+            if (bm_debug_powers)
+                DebugOverlay.TraceResult(tr, 0f);
+            
+            return tr;
+        }
+        
+        
+        tr = Trace.Ray(Player.AimRay, BlinkRange)
+            .Ignore(Player)
+            .Radius(7f)
+            .WithoutTags(BMTags.PhysicsTags.Trigger)
+            .WorldAndEntities()
+            .Run();
+        
+        if (bm_debug_powers)
+            DebugOverlay.TraceResult(tr, 0f);
+
+        return tr;
+    }
+
+    private TraceResult GetDownTrace(TraceResult traceResult)
+    {
+        var startPos = traceResult.EndPosition + traceResult.Normal * 10f;
+        
+        var tr = Trace.Ray(startPos, startPos + Vector3.Down * BlinkFallRange)
+            .Ignore(Player)
+            .WithoutTags(BMTags.PhysicsTags.Trigger)
+            .WorldAndEntities()
+            .Run();
+        
+        if (bm_debug_powers)
+            DebugOverlay.TraceResult(tr, 0f);
+
+        return tr;
+    }
+
+    private Vector3 GetBlinkFinalPosition()
+    {
+        var tr = GetDownTrace(GetBlinkTrace());
+        return tr.EndPosition + tr.Normal*2f;
+    }
+    
+    private void UpdateBlinkVFX()
+    {
+        if (BlinkTarget is null)
+            return;
+
+        var blinkTrace = GetBlinkTrace();
+        BlinkTarget.SetPosition(0, blinkTrace.EndPosition + blinkTrace.Normal * 3f);
+        BlinkTarget.SetPosition(1, GetDownTrace(blinkTrace).EndPosition);
+    }
+
+    private void StartBlink()
+    {
+        BlinkStartPosition = Player.Position;
+        BlinkEndPosition = GetBlinkFinalPosition();
+        IsBlinking = true;
+        TimeUntilBlinkFinished = BlinkStartPosition.Distance(BlinkEndPosition) / BlinkSpeed;
+        
+        BlinkTarget?.Destroy(true);
+        BlinkTarget = null;
+    }
+
+    private void BlinkSimulate()
+    {
+        Player.Position = BlinkStartPosition.LerpTo(BlinkEndPosition, TimeUntilBlinkFinished.Fraction);
+        
+        if (TimeUntilBlinkFinished <= 0)
+            EndBlink();
+    }
+
+    private void EndBlink()
+    {
+        IsBlinking = false;
+    }
+    
+}
--- code/weapons/powers/Powers.cs
+++ code/weapons/powers/Powers.cs
@@ -4,6 +4,9 @@ namespace BattleMages;
 
 public partial class Powers : Carriable
 {
+    [ConVar.Replicated]
+    public static bool bm_debug_powers { get; set; }
+    
     [Net, Predicted]
     public bool IsUsingPower { get; set; }
     
@@ -13,19 +16,41 @@ public partial class Powers : Carriable
     [Net, Predicted]
     private TimeSince TimeSinceReleasedPower { get; set; }
     
+    [Net, Predicted]
+    private bool CancelledInputReleased { get; set; }
+    
     private float MinimumPowerHoldTime => 0.1f;
-    private float Cooldown => 0.25f;
+    private float Cooldown => 0.5f;
     
     private Player Player => Owner as Player;
-
+    
+    
     public override string ViewModelPath => "models/arms/arms_simple.vmdl";
 
     public override void Simulate(IClient client)
     {
+        if (IsBlinking)
+        {
+            BlinkSimulate();
+            return;
+        }
+
         ViewModelEntity?.SetAnimParameter(BMTags.ViewModelAnims.MoveSpeed, Player.Velocity.Length);
 
-        var powerInput = Input.Down(InputButton.SecondaryAttack);
+        if (Input.Down(InputButton.Reload) && IsUsingPower)
+        {
+            CancelPower();
+            return;
+        }
         
+        var powerInput = Input.Down(InputButton.SecondaryAttack);
+
+        if (!powerInput && !CancelledInputReleased)
+            CancelledInputReleased = true;
+        
+        if (!CancelledInputReleased)
+            return;
+
         if (powerInput && !IsUsingPower && TimeSinceReleasedPower > Cooldown)
         {
             BeginUsingPower();   
@@ -39,14 +64,35 @@ public partial class Powers : Carriable
     private void BeginUsingPower()
     {
         IsUsingPower = true;
-        ViewModelEntity?.SetAnimParameter(BMTags.ViewModelAnims.IsUsingPower, true);
+        ViewModelEntity?.SetAnimParameter(BMTags.ViewModelAnims.IsUsingPower, IsUsingPower);
         TimeSinceStartedUsingPower = 0;
+
+        CreateTargetVFX();
     }
 
+    private void CancelPower()
+    {
+        CancelledInputReleased = false;
+        TimeSinceReleasedPower = 0;
+        IsUsingPower = false;
+        ViewModelEntity?.SetAnimParameter(BMTags.ViewModelAnims.IsUsingPower, IsUsingPower);
+        
+        BlinkTarget?.Destroy(true);
+        BlinkTarget = null;
+    }
+    
     private void ReleasePower()
     {
         IsUsingPower = false;
         TimeSinceReleasedPower = 0;
         ViewModelEntity?.SetAnimParameter(BMTags.ViewModelAnims.IsUsingPower, false);
+        
+        StartBlink();
     }
+
+    public override void FrameSimulate(IClient cl)
+    {
+        UpdateBlinkVFX();
+    }
+
 }
